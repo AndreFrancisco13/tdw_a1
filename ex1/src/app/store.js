08e6f98d21c5d304c6bebd1e40c7fdd3a8b97d1c
André Francisco <andrefrancisco@staff-3765.wireless.ua.pt>
2024-12-04 09:47:40 +0000

    fix counter

--- ex1/src/app/store.js
+++ ex1/src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import catReducer from "../components/exercicio4/features/cat/cat-slice";
 import {thunk} from "redux-thunk";
+import counterReducer from "../components/exercicio3/features/counter/counter-slice";
 
 
 export const store = configureStore({
     reducer:{
         cat: catReducer,
+        counter: counterReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
 })
